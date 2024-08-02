1708835a249228c9e61e0a5c86289e61d8175cac
Raattis <raattis@gmail.com>
2024-08-02 10:32:22 +0300

    bigger world

--- terrain.gd
+++ terrain.gd
@@ -10,3 +10,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	rotation.x = get_node("/root/GameState").progress * 100 / scale.z
+	#position.z = -scale.z * 0.45
+	#position.y = -scale.y * 0.24
+	position.x = 0
