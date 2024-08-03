75ebcc3c8f4ad381d0e8958d4c8ab634979c44bd
Raattis <raattis@gmail.com>
2024-08-03 23:46:17 +0300

    bgm

--- audio.gd
+++ audio.gd
@@ -4,6 +4,8 @@ const TEPASTELU = preload("res://sfx/tepastelu.wav")
 @onready var tepastelu_player = $tepastelu_player
 const TURPAAN = preload("res://sfx/turpaan.wav")
 @onready var turpaan_player = $turpaan_player
+@onready var bgm = $bgm
+
 
 func _ready():
 	pass # Replace with function body.
@@ -14,6 +16,10 @@ func _process(delta):
 		if (abs(chicken.up_down) > 0 or abs(chicken.left_right) > 0) and (chicken.linear_velocity.length() > 1 or abs(GameState.progress_velocity) > 0.1):
 			if not tepastelu_player.playing and not GameState.is_stunned():
 				tepastelu_player.play()
+	if GameState.game_started and not GameState.restart_game and not bgm.playing:
+		bgm.play()
+	if GameState.restart_game:
+		bgm.stop()
 
 func crash():
 	if not turpaan_player.playing or turpaan_player.get_playback_position() > 0.5:
